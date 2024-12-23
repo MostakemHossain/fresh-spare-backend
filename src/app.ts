917ceda1eebef51d6ca0f2d838638edce3cf4d1a
MostakemHossain <mostakememon123@gmail.com>
2024-12-24 00:10:29 +0600

    initial project setup

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+
+// express
+const app = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+
+let server: Server;
+const port = 7000;
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    server = app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
+
+process.on('unhandledRejection', () => {
+  console.log(`😈🙉 unhandledRejection is detected. Shutting down...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈🙉 uncaughtException is detected. Shutting down...`);
+  process.exit(1);
+});
