8ba338a15e5b5e2a7e6c7e5cb631ba68a9ca8fb0
MostakemHossain <mostakememon123@gmail.com>
2024-12-24 00:39:23 +0600

    user interface and model complete

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
-
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 // express
 const app = express();
@@ -8,12 +10,18 @@ const app = express();
 // parsers
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  }),
+);
 
 // application routes
 
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Fresh Spare Backend API!!!');
 });
 
 export default app;
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,41 @@
+import { model, Schema } from 'mongoose';
+import { TUSER } from './user.interface';
+
+const UserSchema = new Schema<TUSER>(
+  {
+    name: { type: String, required: [true, 'Name is required'] },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    password: { type: String, required: [true, 'Password is required'] },
+    avatar: { type: String, default: '' },
+    mobile: { type: String, default: '' },
+    refresh_token: { type: String, default: '' },
+    verify_email: { type: Boolean, default: false },
+    last_login_date: { type: Date, default: null },
+    status: {
+      type: String,
+      enum: ['Active', 'Inactive', 'Suspended'],
+      default: 'Active',
+    },
+    role: {
+      type: String,
+      enum: ['Admin', 'User', 'Super_Admin'],
+      default: 'User',
+    },
+    address_details: { type: Schema.Types.ObjectId, ref: 'address' },
+    shopping_cart: { type: Schema.Types.ObjectId, ref: 'cartProduct' },
+    orderHistory: { type: Schema.Types.ObjectId, ref: 'order' },
+    forgot_password_otp: { type: String, default: null },
+    forgot_password_expires: { type: Date },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const UserModel = model<TUSER>('User', UserSchema);
+
+export default UserModel;
